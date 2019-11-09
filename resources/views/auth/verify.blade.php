7b5c1a06d9c9ee45aa4b33f7a03d5da8e23f9d2f
philipko <t6k2b@ugrad.cs.ubc.ca>
2019-11-09 02:20:31 -0800

    Verify Email View

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -16,6 +16,8 @@
 
                     {{ __('Before proceeding, please check your email for a verification link.') }}
                     {{ __('If you did not receive the email') }}, <a href="{{ route('verification.resend') }}">{{ __('click here to request another') }}</a>.
+                    <br>
+                    <br>{{ __('The Dashboard feature is very useful so verifying your email will be really good!') }}
                 </div>
             </div>
         </div>
